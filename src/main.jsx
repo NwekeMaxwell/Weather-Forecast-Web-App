7d0dd2dbc3a6b5c80b8f48bbe209c5a2c004140c
NwekeMaxwell <nwekemaxwell37@gmail.com>
2024-03-27 22:00:44 +0100

    feat: add weather api context

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { Toaster } from "react-hot-toast";
+import { WeatherContextProvider } from "./lib/contexts/weatherContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Toaster />
-    <App />
+    <WeatherContextProvider>
+      <App />
+    </WeatherContextProvider>
   </React.StrictMode>
 );
