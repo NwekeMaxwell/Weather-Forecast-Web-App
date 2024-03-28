e70804428b47f015db225cee376281953ae0744d
NwekeMaxwell <nwekemaxwell37@gmail.com>
2024-03-28 16:09:04 +0100

    feat: add autocomplete search functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,76 +1,19 @@
-import { useState } from "react";
 import "./App.css";
-import search from "./assets/icons/search.svg";
 import { useWeatherContext } from "./lib/contexts/weatherContext";
-import BackgroundLayout from "./components/Background";
 import WeatherCard from "./components/WeatherCard";
 import MiniCard from "./components/MiniCard";
-import { LoaderIcon } from "react-hot-toast";
+import Search from "./components/search";
 
 function App() {
-  const [currentWeather, setCurrentWeather] = useState(null);
-  const [forecast, setForecast] = useState(null);
-
-  const handleOnSearchChange = (searchData) => {
-    const [lat, lon] = searchData.value.split(" ");
-
-    const currentWeatherFetch = fetch(
-      `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
-    );
-    const forecastFetch = fetch(
-      `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
-    );
-
-    Promise.all([currentWeatherFetch, forecastFetch])
-      .then(async (response) => {
-        const weatherResponse = await response[0].json();
-        const forcastResponse = await response[1].json();
-
-        setCurrentWeather({ city: searchData.label, ...weatherResponse });
-        setForecast({ city: searchData.label, ...forcastResponse });
-      })
-      .catch(console.log);
-  };
-
-  const [input, setInput] = useState("");
-  const { weather, thisLocation, values, place, setPlace, loading } =
+  const { weather, thisLocation, values } =
     useWeatherContext();
-
-  const submitCity = () => {
-    setPlace(input);
-    setInput("");
-  };
 
   return (
     <>
       <div className="w-screen text-white gradient min-h-screen h-[100%] lg:px-8 overflow-hidden">
-        {/* <Search onSearchChange={handleOnSearchChange}/> */}
         <nav className="w-full flex flex-col md:flex-row justify-between items-center pt-6 px-6 gap-4">
           <h1 className="font-bold tracking-wide text-3xl">Weather Forecast</h1>
-          <div className="bg-white w-[15rem] overflow-hidden shadow-2xl rounded-lg flex items-center p-2 gap-2">
-            <img
-              src={search}
-              onClick={submitCity}
-              alt="search"
-              className="w-[1.5rem] h-[1.5rem] active:scale-75 transition-all duration-500"
-            />
-            <input
-              onKeyUp={(e) => {
-                if (e.key === "Enter") {
-                  // sumit the form
-                  submitCity();
-                }
-              }}
-              type="text"
-              placeholder="Search city"
-              className="focus:outline-none w-full text-[#212121] text-lg"
-              value={input}
-              onChange={(e) => setInput(e.target.value)}
-            />
-            {loading && (
-              <LoaderIcon style={{ width: "20px", height: "20px" }} />
-            )}
-          </div>
+          <Search />
         </nav>
         <main className="w-full flex flex-col md:flex-row flex-wrap gap-8 py-4 items-center justify-center">
           <WeatherCard
---
+++ src/components/search.jsx
@@ -0,0 +1,87 @@
+import React, { useState, useEffect } from "react";
+import { useWeatherContext } from "../lib/contexts/weatherContext";
+import { LoaderIcon } from "react-hot-toast";
+import searchIcon from "../assets/icons/search.svg";
+import axios from "axios";
+
+const Search = () => {
+  const [search, setSearch] = useState("");
+  const [options, setoptions] = useState([]);
+  const { setPlace, loading } = useWeatherContext();
+
+  const submitCity = () => {
+    // console.log(input);
+    setPlace(search);
+    setSearch("");
+  };
+  //fetch cities function utilizing rapid api GeoDB cities API
+  const fetchOptions = async () => {
+    const options = {
+      method: "GET",
+      url: "https://wft-geo-db.p.rapidapi.com/v1/geo/cities",
+      params: {
+        namePrefix: search,
+      },
+      headers: {
+        "X-RapidAPI-Key": "0b025d2537msh80c1f91c6260e47p1be4eejsn7c49c199c834",
+        "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+      },
+    };
+
+    try {
+      const response = await axios.request(options);
+      setoptions(response.data.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    //fetch cities on every user interaction with the search input
+    fetchOptions();
+  }, [search]);
+
+  return (
+    <div className="relative">
+      <div className="w-[15rem] bg-white overflow-hidden shadow-2xl rounded-lg flex items-center p-2 gap-2">
+        <input
+          onKeyUp={(e) => {
+            if (e.key === "Enter") {
+              submitCity();
+            }
+          }}
+          type="text"
+          placeholder="Search city"
+          className="focus:outline-none w-full text-[#212121] text-lg"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <img
+          src={searchIcon}
+          onClick={submitCity}
+          alt="search"
+          className="w-[1.5rem] h-[1.5rem] active:scale-75 transition-all duration-500 cursor-pointer"
+        />
+        {loading && <LoaderIcon style={{ width: "20px", height: "20px" }} />}
+      </div>
+      {search.length > 0 && (
+        <div className="absolute left-0 top-[110%] rounded z-40 bg-white w-full max-h-[25vh] overflow-y-scroll">
+          {options.map((item, index) => {
+            return (
+              <div
+                key={index}
+                onClick={() => {
+                  setSearch(item.city);
+                }}
+                className="px-1 py-2 text-black border-b border-solid hover:bg-gray-200 cursor-pointer"
+              >
+                {`${item.city}, ${item.country}`}
+              </div>
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
+};
+export default Search;
